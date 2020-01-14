d0545cb495326fd174a63d91ff84eb9304603202
mrtalhim <mrtalhim@gmail.com>
2020-01-14 22:03:41 +0700

    tambah chart + menu di profil, tambah edit profil

---
+++ lib/screens/edit-profile-screen.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+
+Color textColor = Color(0xff6980C5);
+
+class EditProfileScreen extends StatefulWidget {
+  @override
+  _EditProfileState createState() => _EditProfileState();
+}
+
+class _EditProfileState extends State<EditProfileScreen> {
+  var _visiblePassword = false;
+
+  setVisiblePassword() {
+    setState(() {
+      _visiblePassword = !_visiblePassword;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Image.asset('assets/images/logo-flat.png'),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(width: 2, color: textColor),
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                margin: EdgeInsets.only(bottom: 20, left: 20, right: 20),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Nama',
+                    hintStyle: TextStyle(
+                      color: textColor,
+                    ),
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(width: 2, color: textColor),
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                margin: EdgeInsets.only(bottom: 20, left: 20, right: 20),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'No. KTP',
+                    hintStyle: TextStyle(
+                      color: textColor,
+                    ),
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(width: 2, color: textColor),
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                margin: EdgeInsets.only(bottom: 20, left: 20, right: 20),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Phone',
+                    hintStyle: TextStyle(
+                      color: textColor,
+                    ),
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(width: 2, color: textColor),
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                margin: EdgeInsets.only(bottom: 20, left: 20, right: 20),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                    hintStyle: TextStyle(
+                      color: textColor,
+                    ),
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(width: 2, color: textColor),
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                margin: EdgeInsets.only(bottom: 20, left: 20, right: 20),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    hintStyle: TextStyle(
+                      color: textColor,
+                    ),
+                    border: InputBorder.none,
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        (_visiblePassword == false
+                            ? Icons.visibility
+                            : Icons.visibility_off),
+                        color: textColor,
+                      ),
+                      onPressed: () {
+                        setVisiblePassword();
+                      },
+                    ),
+                  ),
+                  obscureText: _visiblePassword == false ? true : false,
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                margin: EdgeInsets.only(bottom: 20, left: 20, right: 20),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: textColor,
+                ),
+                child: FlatButton(
+                  onPressed: () {},
+                  child: Text(
+                    'NEXT',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/home-screen.dart
+++ lib/screens/home-screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:makaryo_mobile/color.dart';
 import 'package:makaryo_mobile/components/home-section/home-agenda.dart';
+import 'package:makaryo_mobile/screens/dummyData.dart';
+import 'package:makaryo_mobile/screens/edit-profile-screen.dart';
 import 'package:makaryo_mobile/screens/login-screen.dart';
+import 'package:pie_chart/pie_chart.dart';
 import '../components/home-section/home-category.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -98,6 +101,8 @@ class ProfilScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Map<String, double> dataMap = Map.fromIterable(kategoriList,
+        key: (k) => k.getKategoriName(), value: (k) => k.getPercentage());
     double deviceWidth = MediaQuery.of(context).size.width;
     double deviceHeight = MediaQuery.of(context).size.height;
     return SingleChildScrollView(
@@ -105,41 +110,118 @@ class ProfilScreen extends StatelessWidget {
         color: backgroundColor,
         child: Column(
           children: <Widget>[
+            AppBar(
+              backgroundColor: Color.fromRGBO(0, 0, 0, 0),
+              elevation: 0,
+              actions: <Widget>[
+                PopupMenuButton(
+                  onSelected: (selected) => _select(context, selected),
+                  icon: Icon(
+                    Icons.more_vert,
+                    color: Colors.black87,
+                  ),
+                  itemBuilder: (context) => <PopupMenuEntry>[
+                    PopupMenuItem(
+                      value: 0,
+                      child: Text("Ubah Profil"),
+                    ),
+                    PopupMenuItem(
+                      value: 1,
+                      child: Text("Log Out"),
+                    ),
+                  ],
+                ),
+              ],
+            ),
             Container(
-              padding: EdgeInsets.only(top: 80),
+              margin: EdgeInsets.only(top: 20),
+              padding: EdgeInsets.only(left: 20, right: 20),
               child: Column(
                 children: <Widget>[
                   Center(
                     child: CircleAvatar(
+                      child: Icon(
+                        Icons.person,
+                        size: (1 / 6) * deviceWidth,
+                      ),
                       backgroundColor: primaryColor,
                       radius: (1 / 8) * deviceWidth,
                     ),
                   ),
-                  Text(
-                    "Mohammad Talha",
-                    style: Theme.of(context).textTheme.title,
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                          "Mohammad Talha",
+                          style: Theme.of(context).textTheme.title,
+                        ),
+                        Text(
+                          "mx@email.com",
+                          style: Theme.of(context).textTheme.subtitle,
+                        ),
+                      ],
+                    ),
                   ),
-                  FlatButton(
-                    child: Text("LOG OUT"),
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => LoginScreen(),
-                          ));
-                    },
-                  )
                 ],
               ),
             ),
             Container(
-                child: Center(
-              child: Text("Profil"),
-            ))
+              margin: EdgeInsets.only(top: 20),
+              padding: EdgeInsets.only(left: 20, right: 20),
+              child: Column(
+                children: <Widget>[
+                  Card(
+                    child: Container(
+                      margin: EdgeInsets.all(20),
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            padding: EdgeInsets.all(10),
+                            height: (1 / 4) * deviceHeight,
+                            child: PieChart(
+                              chartType: ChartType.ring,
+                              dataMap: dataMap,
+                              showLegends: false,
+                            ),
+                          ),
+                          Text("Progres Belajar 75%"),
+                          RaisedButton(
+                            onPressed: () {},
+                            child: Text("Lanjut Belajar"),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
           ],
         ),
       ),
     );
+  }
+
+  void _select(BuildContext context, var selected) {
+    switch (selected) {
+      case 0:
+        gotoEditProfil(context);
+        break;
+      case 1:
+        gotoLogin(context);
+        break;
+    }
+  }
+
+  gotoLogin(BuildContext context) {
+    Navigator.pop(context);
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => LoginScreen()));
+  }
+
+  gotoEditProfil(BuildContext context) {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => EditProfileScreen()));
   }
 }
