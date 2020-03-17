16138fca90a8f53c84869a2be16e9df0ac620e76
Rifqi Eka Hardianto <ekarifqi1@gmail.com>
2020-03-17 22:26:29 +0700

    menambahkan auth service

--- lib/auth_service.dart
+++ lib/auth_service.dart
@@ -1,1 +1,55 @@
+import 'package:firebase_auth/firebase_auth.dart';
 
+abstract class AuthService {
+  final auth = FirebaseAuth.instance;
+
+  Future signInAnonimouslyHandle() async {
+    try {
+      await auth
+          .signInAnonymously()
+          .then(
+            (value) => print('success sign in'),
+          )
+          .catchError((e) => print(e));
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future signOutAnonimouslyHandle() async {
+    try {
+      await auth
+          .signOut()
+          .then((value) => print('success sign out'))
+          .catchError((e) => print(e));
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future signInWithEmailAndPasswordHandle(email, password) async {
+    try {
+      await auth
+          .signInWithEmailAndPassword(email: email, password: password)
+          .then((value) => print('success sign in'))
+          .catchError((e) => print(e));
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future registerWithEmailAndPasswordHandle(email, password) async {
+    try {
+      await auth
+          .createUserWithEmailAndPassword(email: email, password: password)
+          .then((value) => print('success register'))
+          .catchError((e) => print(e));
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  getCurrentUser() async {
+    return await auth.currentUser();
+  }
+}
--- lib/screens/login-screen.dart
+++ lib/screens/login-screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:makaryo_mobile/auth_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:makaryo_mobile/screens/admin-home-screen.dart';
 import 'package:makaryo_mobile/screens/home-screen.dart';
 import 'package:makaryo_mobile/screens/register-screen.dart';
