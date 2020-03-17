37345d9ac3f3edd8f8f7afa96987e080c4f638ef
Rifqi Eka Hardianto <ekarifqi1@gmail.com>
2020-03-17 22:11:07 +0700

    menghapus chewie, digantikan dengan video_player

--- lib/main.dart
+++ lib/main.dart
@@ -53,34 +53,34 @@ class MaterialAppWithTheme extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Video Player'),
-      ),
-      body: ListView(
-        children: <Widget>[
-          ChewieListItem(
-            videoPlayerController: VideoPlayerController.asset(
-              'videos/IntroVideo.mp4',
-            ),
-            looping: true,
-          ),
-          ChewieListItem(
-            videoPlayerController: VideoPlayerController.network(
-              'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
-            ),
-          ),
-          ChewieListItem(
-            // This URL doesn't exist - will display an error
-            videoPlayerController: VideoPlayerController.network(
-              'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/error.mp4',
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
+// class MyHomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Video Player'),
+//       ),
+//       body: ListView(
+//         children: <Widget>[
+//           ChewieListItem(
+//             videoPlayerController: VideoPlayerController.asset(
+//               'videos/IntroVideo.mp4',
+//             ),
+//             looping: true,
+//           ),
+//           ChewieListItem(
+//             videoPlayerController: VideoPlayerController.network(
+//               'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+//             ),
+//           ),
+//           ChewieListItem(
+//             // This URL doesn't exist - will display an error
+//             videoPlayerController: VideoPlayerController.network(
+//               'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/error.mp4',
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
